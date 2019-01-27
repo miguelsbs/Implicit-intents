810b63649889b006f9bb038ef439dca4fd5fa787
Miki <miguelsanblassanchez@gmail.com>
2019-01-27 17:05:10 +0100

    Task2: Implementar el boton para abrir un sitio web

--- app/src/main/java/com/example/miki/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/miki/implicitintents/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.miki.implicitintents;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebsiteEditText = findViewById(R.id.website_edittext);
     }
 
     public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else{
+            Log.d("Implicit-intents", "Can´t handle this intent!");
+        }
+
+
     }
 
     public void openLocation(View view) {

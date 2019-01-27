9a73508b2f8e1bedefa90e622c262bf845341265
Miki <miguelsanblassanchez@gmail.com>
2019-01-27 12:59:11 +0100

    Task1: Crear el proyecto y el diseño

---
+++ app/src/main/java/com/example/miki/implicitintents/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.miki.implicitintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
+    }
+}
